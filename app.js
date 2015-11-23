b681b1edd6e8acd15b0be5df9f3e40921a83210a
mlefree <mathieu.leprevost@apps4pro.com>
2015-11-23 11:50:26 +0000

    (koding) json & config....

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ var app = express();
 console.log("server launched");
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 8080);
+
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+/*
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}*/
